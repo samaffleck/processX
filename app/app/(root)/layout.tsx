75eb627195fe9b27547a8bf4629570147ce54ecf
samaffleck <sam.affleck@hotmail.com>
2025-11-02 17:07:45 +0000

    Add: move page to Root folder

---
+++ app/app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ProcessX",
+  description: "Web-based process simulation",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
